f2acf5084f7a92c55fc5440d1fa630e674600ea6
wira ardi <wiraardi79@gmail.com>
2022-05-27 16:54:08 +0700

    add project bwatartup register user

---
+++ handler/user.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	service user.Service
+}
+
+func NewUserHandler(service user.Service) *userHandler {
+	return &userHandler{
+		service: service,
+	}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		webResponse := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, webResponse)
+		return
+	}
+
+	newUser, err := h.service.RegisterUser(input)
+	if err != nil {
+		webResponse := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, webResponse)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, "tokentokentoken")
+
+	webResponse := helper.APIResponse("Success Register", http.StatusOK, "success", formatter)
+	c.JSON(http.StatusOK, webResponse)
+}
